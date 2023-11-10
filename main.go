b86c91e5b6265cac36f78627a9d72455c44a04bf
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-11-09 22:57:52 -0800

    Added tests for countlines functionality

--- main.go
+++ main.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	// calling the count function to print the number of words
-	// received form the standard input and printing it out
-	fmt.Println(count(os.Stdin))
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// parse the flags provided by the user
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines))
 }
 
 // a function that counts command line args by words
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// an io Reader is a go type (interface) from which u can read data
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// if the count lines flag is not set, we want to count words so we define
+	// the scanner split type to words (default behavior is to split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Defining a counter
 	wc := 0
